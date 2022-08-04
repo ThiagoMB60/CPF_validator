ec6f12e8714c5ec8c901c6b5aa1fd3887d115544
ThiagoMB60 <thiagomoreira60@gmail.com>
2022-08-03 23:38:21 -0300

    realizando calculo regressivo dos elementos

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ function validaCpf(cpf) {
 }
 
 validaCpf.prototype.valida = function () {
-  if (!this.cleanCpf) return false;
+  if (!this.cleanCpf || this.cleanCpf.length !== 11) return false;
+
+  const cpfParcial = this.cleanCpf.slice(0, -2);
+  const digito1 = this.criaDigito(cpfParcial);
   return true;
 }
+
+validaCpf.prototype.criaDigito = function (cpfParcial) {
+  const arrayDigits = Array.from(cpfParcial)
+  let regressivo = arrayDigits.length + 1;
+  const digito = arrayDigits.reduce((acumulador, value) => {
+    acumulador += (regressivo * Number(value))
+    regressivo--;
+    return acumulador;
+  }, 0)
+  console.log(digito)
+}
+
+
 const cpfTeste = '126.200.796-83';
 const cpf = new validaCpf(cpfTeste);
 console.log(cpf.valida());
